78d5cb529936c4e12407040e4d5d9a732728cd64
unknown <joaopedrodacunha.santos@gmail.com>
2025-07-15 20:42:01 -0300

    Create JImagePanel end Product

---
+++ src/main/java/br/com/project/storage/back/model/Product.java
@@ -0,0 +1,5 @@
+package br.com.project.storage.back.model;
+
+public class Product {
+    
+}
---
+++ src/main/java/br/com/project/storage/front/panel/JImagePanel.java
@@ -0,0 +1,7 @@
+package br.com.project.storage.front.panel;
+
+import javax.swing.JPanel;
+
+public class JImagePanel extends JPanel{
+    
+}
