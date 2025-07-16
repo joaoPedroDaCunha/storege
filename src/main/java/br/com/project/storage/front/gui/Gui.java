65191b64c3b03491065419b2d9ed1b408830712f
unknown <joaopedrodacunha.santos@gmail.com>
2025-07-15 21:02:36 -0300

    basic implementations Gui

--- src/main/java/br/com/project/storage/front/gui/Gui.java
+++ src/main/java/br/com/project/storage/front/gui/Gui.java
@@ -1,8 +1,45 @@
 package br.com.project.storage.front.gui;
 
 import javax.swing.JFrame;
+
+import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class Gui extends JFrame implements WindowListener{
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'windowActivated'");
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'windowClosed'");
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'windowClosing'");
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'windowDeactivated'");
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'windowDeiconified'");
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'windowIconified'");
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'windowOpened'");
+    }
     
 }
