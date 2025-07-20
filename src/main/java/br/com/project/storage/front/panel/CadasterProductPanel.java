ae5419f7326fec8beefa9f4149406a76e8c90d55
unknown <joaopedrodacunha.santos@gmail.com>
2025-07-19 21:44:26 -0300

    Create CadasterProductPanel

---
+++ src/main/java/br/com/project/storage/front/panel/CadasterProductPanel.java
@@ -0,0 +1,11 @@
+package br.com.project.storage.front.panel;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+
+public class CadasterProductPanel extends JPanel{
+    public CadasterProductPanel(){
+        setLayout(new FlowLayout());
+    }
+}
