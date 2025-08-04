16135628e5b0c05b18efb3ae382eebb0038bc019
unknown <joaopedrodacunha.santos@gmail.com>
2025-08-03 23:15:47 -0300

    Basic controller Product

--- src/main/java/br/com/project/storage/back/controller/ProductController.java
+++ src/main/java/br/com/project/storage/back/controller/ProductController.java
@@ -1,5 +1,26 @@
 package br.com.project.storage.back.controller;
 
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.project.storage.back.model.Product;
+import br.com.project.storage.back.service.ProductService;
+
 public class ProductController {
+    private @Autowired ProductService productService;
     
+    public Set<Product> getAll(){
+        return productService.getAll();
+    }
+    public void post(Product product){
+        productService.Post(product);
+    }
+    public Optional<Product> getByName(Integer idProduct){
+        return productService.getByName(idProduct);
+    }
+    public void deleteById(Integer idProduct){
+        productService.delete(idProduct);
+    }
 }
